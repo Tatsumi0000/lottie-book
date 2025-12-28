515e51ad64f552a6d055e120e29b1803bac68a67
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2025-12-29 00:17:15 +0900

    Vuetifyを呼び出せるように追加

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
+import TheHeader from "./components/TheHeader.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <VApp>
+    <TheHeader />
+    <VMain>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+      <HelloWorld msg="Vite + Vue" />
+    </VMain>
+  </VApp>
 </template>
 
 <style scoped>
