f78559683409ab0398723da7d939691a7b2b1f72
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2025-12-29 00:16:59 +0900

    ヘッダーを追加しつつダークモードを切り替えるスイッチも追加

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { useTheme } from "vuetify";
+import { VAppBar, VSwitch } from "vuetify/components";
+
+const theme = useTheme();
+const isDarkTheme = ref(theme.global.current.value.dark);
+const changeTheme = () => {
+  theme.global.name.value = isDarkTheme.value ? "dark" : "light";
+};
+</script>
+
+<template>
+  <VAppBar title="lottie-title">
+    <template v-slot:append>
+      <VSwitch
+        v-model="isDarkTheme"
+        @update:model-value="changeTheme"
+        :prepend-icon="isDarkTheme ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+        hide-details
+        inset
+        class="mr-auto"
+      />
+    </template>
+  </VAppBar>
+</template>
+
+<style scoped></style>
