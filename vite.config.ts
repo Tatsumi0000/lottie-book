5cb06124a01268d1878ffe43595d8edd4e38b85a
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2026-01-13 01:37:18 +0900

    deno install npm:vite-plugin-vuetify

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import ViteFonts from "unplugin-fonts/vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath } from "node:url";
+import vuetify from "vite-plugin-vuetify";
 
-  export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/lottie-books/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/lottie-books/" : "/",
   plugins: [
     vue(),
+    vuetify({ autoImport: true }),
     ViteFonts({
       fontsource: {
         families: [
